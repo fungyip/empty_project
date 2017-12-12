6f33c1380af2d9d05a753e291a47386e6d5e4744
J. Muller <jomuller@users.noreply.github.com>
2017-12-12 10:01:00 +0100

    Refactor function and Roxygen documentation
    Open URL is platform agnostic

--- functions/make_reports.R
+++ functions/make_reports.R
@@ -1,6 +1,19 @@
-#opts_knit$set(root.dir = '../.')
-report <- function(rmd, n_file, open_file = default_open_file,
-    report_dir = "reports", rmds_dir = "rmds") {
+#' Produce reports
+#'
+#' Produce html reports from a rmd file. It's mainly a wrapper arrond [rmarkdown::render()]
+#'
+#' @return Return invisibly the file path of the produced report
+#' @param rmd name of the rmd file
+#' @param n_file prefix numbering of the output file
+#' @param open_file open the html file in the default browser once produced. By default, `TRUE`.
+#' @param report_dir target directory for the reports
+#' @param rmds_dir source directery for the `.Rmds` files. By default, the directory *rmds*.
+#' @export
+#' @importFrom rmarkdown render
+#' @md
+#'
+report <- function(rmd, n_file, open_file = TRUE,
+                   report_dir = "reports", rmds_dir = "rmds") {
 
   if(!require(rmarkdown)) stop("Install rmarkdown package")
 
@@ -10,16 +23,18 @@ report <- function(rmd, n_file, open_file = default_open_file,
   base_name <- sub(pattern = ".Rmd", replacement = "", x = basename(rmd_path))
   html_name <- paste0(base_name, ".html")
 
-  if(!missing(n_file)){
+  if(missing(n_file)){
+    file_name <- html_name
+  } else {
     # Make nfiles with always 2 digits
     n_file <- ifelse(as.integer(n_file) < 10, paste0("0", n_file), n_file)
     file_name <- paste0(n_file, "-", html_name)
-  } else {
-      file_name <- html_name
   }
+
   # Produce the file. Simple wrapper of the render function
   rmarkdown::render(
     input = rmd_path,
+    encoding = "UTF-8",
     output_format = html_document(
       toc = TRUE,
       toc_depth = 1,
@@ -28,11 +43,11 @@ report <- function(rmd, n_file, open_file = default_open_file,
     output_file = file_name,
     output_dir = report_dir,
     envir = new.env()
-    )
+  )
 
-  if(open_file & Sys.info()[1] == "Darwin") {
-    result_path <- file.path(report_dir, file_name)
-    system(command = paste("Open", result_path))
-  }
+  result_path <- file.path(report_dir, file_name)
 
+  if(open_file) browseURL(result_path)
+
+  invisible(result_path)
 }
